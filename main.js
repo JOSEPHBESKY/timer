dbd077ecf82a00eb56c1efd08c26e5d170807170
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-06-24 15:15:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,74 +6,74 @@ var start=document.getElementById('start')
 var swap=document.getElementById('swap');
 var swap1=document.getElementById('swap1');
 var reset=document.getElementById('reset');
-var inputbox=document.getElementById('btn1')
-inputbox.addEventListener('mouseout',show)
+var inputbox=document.getElementById('btn1');
+inputbox.addEventListener('mouseout',show);
 function show() {
-    inputvalue=inputbox.value
+    inputvalue=inputbox.value;
     console.log(typeof inputvalue);
 var startminute=parseInt(inputvalue);
 let time=startminute*60;
  let time1=startminute*60;
  start.addEventListener('click',function timerstart () {
-     clear=setInterval(timer,1000)
-     start.setAttribute('disabled','true')
+     clear=setInterval(timer,1000);
+     start.setAttribute('disabled','true');
 })
 swap1.addEventListener('click',function timerstart () {
     
-    clearInterval(swap)
-     clear=setInterval(timer,1000)
+    clearInterval(swap);
+     clear=setInterval(timer,1000);
 })
 swap.addEventListener('click',function timerstart1 () {
-    clearInterval(clear)
-    swap=setInterval(timer1,1000)
+    clearInterval(clear);
+    swap=setInterval(timer1,1000);
 })
 reset.addEventListener('click',function resets() {
-    clearInterval(clear)
-    clearInterval(swap)
-    var minutes=00
-    var seconds=00
-    minute1.innerHTML=minutes
-    minute2.innerHTML=minutes
-    second1.innerHTML=seconds
-     second2.innerHTML=seconds
-        let start=document.getElementById('start')
-    start.setAttribute('disabled','false')
+    clearInterval(clear);
+    clearInterval(swap);
+    var minutes=00;
+    var seconds=00;
+    minute1.innerHTML=minutes;
+    minute2.innerHTML=minutes;
+    second1.innerHTML=seconds;
+     second2.innerHTML=seconds;
+        let start=document.getElementById('start');
+    start.disabled=false;
 })
 function timer(){
     var minutes=Math.floor(time/60);
-    var seconds=time%60
-     minute2.innerHTML=minutes
+    var seconds=time%60;
+     minute2.innerHTML=minutes;
     if (minutes<5) {
-        minutes="0"+minutes
+        minutes="0"+minutes;
     }
     if (minutes==00 && seconds==00) {
         
-        clearInterval(clear)
-        time=stop
+        clearInterval(clear);
+        time=stop;
     }
     second2.innerHTML=seconds
     if (seconds<10) {
-        seconds="0"+seconds
+        seconds="0"+seconds;
     }
-    time--
+    time--;
     
 }
 function timer1(){
     var minutes1=Math.floor(time1/60);
-    var seconds2=time1%60
-    minute1.innerHTML=minutes1
+    var seconds2=time1%60;
+    minute1.innerHTML=minutes1;
     if (minutes1<5) {
-        minutes1="0"+minutes1
+        minutes1="0"+minutes1;
     }
     if (minutes1==00 && seconds2==00) {
-        clearInterval(swap)
-        time=stop
+        clearInterval(swap);
+        time=stop;
     }
-    second1.innerHTML=seconds2
+    second1.innerHTML=seconds2;
     if (seconds2<10) {
-        seconds2="0"+seconds2
+        seconds2="0"+seconds2;
     }
-    time1--
+    time1--;
 }
 
 }
